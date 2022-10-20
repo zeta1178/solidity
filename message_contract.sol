dd5e5d3cb9c89c82edd9e9379c61aeaee4a0bebb
Remix Workspace <>
2022-10-19 21:29:43 -0400

    update setter and getters

---
+++ message_contract.sol
@@ -0,0 +1,23 @@
+
+pragma solidity ^0.5.0;
+
+contract CustomerAccount {
+    address owner;
+    bool isNewAccount;
+    uint accountBalance;
+    string customerName;
+    string customerLastName;
+
+    function getInfo() view public returns(address, bool, uint, string memory, string memory) {
+        return (owner, isNewAccount, accountBalance, customerName, customerLastName);
+    }
+
+    function setInfo(address newOwner, bool newAccountStatus, uint
+    newAccountBalance, string memory newCustomerName, string memory newCustomerLastName) public {
+        owner = newOwner;
+        isNewAccount = newAccountStatus;
+        accountBalance = newAccountBalance;
+        customerName = newCustomerName;
+        customerLastName = newCustomerLastName;
+    }
+}
